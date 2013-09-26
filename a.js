03f5bfbe2c8651f1351f32cebb03a3e228aa3195
g <g@g.(none)>
2013-09-25 20:58:28 -0400

    addingn something

--- a.js
+++ a.js
@@ -1,6 +1,12 @@
-console.log("HELLO WORLD, <3 VMX");
-
-
 VMX.callback = function(detections){
+  console.log("inside the callback");
   console.log(detections);
 }
+
+
+vmx.enter("fist").moveto({region}).moveto({otherregion});
+
+vmx.on_leave("fist", function(detection){
+
+});
+
